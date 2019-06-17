23243f50b0ca25734898b644a6405b5bf189ce07
SatoshiFPV <jeremy.noverraz@gmail.com>
2019-06-17 03:34:03 +0200

    Create MoistureV1.2.ino

---
+++ MoistureV1.2.ino
@@ -0,0 +1,70 @@
+/*********************************************************************************
+ *          ********************************************************            **
+ *          **  Moisture capacitive sensor V1.2 Automatic garden  **            **             
+ *          ********************************************************            **                             
+ *                                                                              **
+ *  Author : Jérémy Noverraz                                                    **
+ *  Date of creation : 17/06/2019 01:05AM UTC +1                                **
+ *  Specification : Read a moisture capacitive sensor with an arduino uno       **     
+ *   et allumer un relais 230/5V après un certain seuil de secheresse atteint   **
+ *                                                                              **
+ *********************************************************************************/
+
+
+  /* * * * * * * * * * * * *
+   * * * * * * * * * * * * *
+   * *                   * *
+   * *    VARIABLES      * * 
+   * *                   * * 
+   * * * * * * * * * * * * *
+   * * * * * * * * * * * * */
+
+  int analogPin = A0;
+  int relay = 13;
+  int moisture;
+  int percentage;
+  int D = 500;
+  int irrigationTime = 8000; /*Temps d'irrigation à changer selon la capacité d'evacuation de votre pompe et donc de la quantitée d'eau nécéssaire lord d'un arrosage /*
+
+  /* * * * * * * * * * * * * * * * * * * * * * 
+   * * * * * * * * * * * * * * * * * * * * * *  
+   * *                                     * *
+   * *   VARIABLES POUR LA CALIBRATION     * * 
+   * *                                     * * 
+   * * * * * * * * * * * * * * * * * * * * * * 
+   * * * * * * * * * * * * * * * * * * * * * */
+
+   int map_low = 619; /* valeure maximal quand la sonde est à l'air libre */
+   int map_high = 241; /* valeure maximal quand la sonde est trempé dans un verre d'eau */
+
+
+void setup() {
+Serial.begin(9600);
+pinMode(relay, OUTPUT);
+digitalWrite(relay, HIGH); /* On éteint la pompe */
+delay(D);
+} 
+void loop() {
+
+  moisture = analogRead(analogPin);
+  Serial.print("Raw data: ");
+  Serial.print(moisture);
+
+  percentage = map(moisture, map_low, map_high, 0, 100); /* On utilise la fonction MAP pour */
+
+  Serial.print( " | " );
+  Serial.print(percentage);
+  Serial.print(" %");
+  Serial.println("");
+
+  if (moisture > 500 ){
+    digitalWrite(relay, LOW); /* Si le pourcentage est inférieur à 40% d'humidité, On allume la pompe */
+    delay(irrigationTime);
+    digitalWrite(relay, HIGH);
+    }
+
+
+  delay(1000);
+  
+
+}
